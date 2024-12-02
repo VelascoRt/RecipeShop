3ab70f421e1312e8437b523e186c2fec9d7f9bf2
VelascoRt <velascogmac@gmail.com>
2024-12-02 15:01:45 -0600

    Laptop config

--- final/settings.py
+++ final/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
 'default': {
     'ENGINE': 'django.db.backends.mysql',
     'NAME': 'recipes',
-    'USER': 'recipead',
+    'USER': 'recipeadmin',
     'PASSWORD': 'pass',
     'HOST': 'localhost',
     'PORT': '3306',
